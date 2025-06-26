f32334ab5c97bab36b40940f1359a7e2e8171949
caaaiouw <c.oliveira1696@gmail.com>
2025-06-26 19:59:56 -0300

    Add files via upload

---
+++ Contador/src/contadorException/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package contadorException;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
